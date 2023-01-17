ed58c089a4f9a16d2cdb6d5f09592595608646ca
Austin Barr <88349487+austinthebarr1@users.noreply.github.com>
2023-01-17 15:11:10 -0800

    Add Joes IP

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_cosmosdb_account" "this" {
   offer_type                    = "Standard"
   kind                          = var.cosmos_api == "mongo" ? "MongoDB" : "GlobalDocumentDB"
   public_network_access_enabled = true
-  ip_range_filter               = "0.0.0.0,159.49.0.0/24,4.35.246.0/24,50.228.14.0/24,63.236.113.225/24,63.236.116.0/24,97.113.149.9"
+  ip_range_filter               = "0.0.0.0,159.49.0.0/24,4.35.246.0/24,50.228.14.0/24,63.236.113.225/24,63.236.116.0/24,97.113.149.9,98.240.185.62"
 
 
   enable_automatic_failover       = var.auto_failover
